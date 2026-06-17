e11933d3906088ae266631c9a133c3519f5c3e31
Hasnaoui <amar.hasnaoui@edu.univ-paris13.fr>
2026-06-17 10:46:16 +0200

    1st push

--- infra_snowflake/providers.tf
+++ infra_snowflake/providers.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.96"  # Use the latest stable version
+      version = "~> 0.96"
     }
   }
 }
 
 provider "snowflake" {
-  account           = var.snowflake_account
+  organization_name = var.snowflake_org
+  account_name      = var.snowflake_account
   user              = var.snowflake_user
   password          = var.snowflake_password
   role              = var.snowflake_role
